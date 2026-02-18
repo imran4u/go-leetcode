bb0117da1df61c6c1d454943c26fe56216629208
Imran Ali <er.imran4u@gmail.com>
2026-02-18 16:44:02 +0530

    complete the leetcode-1 problem

--- cmd/l-1/main.go
+++ cmd/l-1/main.go
@@ -1,15 +1,48 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	// nums := []int{2, 7, 11, 15}
+	// target := 9
+	// ans = [0,1]
+
+	nums := []int{3, 2, 4}
+	target := 6
+	// Output: [1,2]
+
+	result := twoSum(nums, target)
+	fmt.Println("result", result)
 
 }
 
 func twoSum(nums []int, target int) []int {
-	result := []int{}
-	maps := make(map[int]int)
-
-	for i, v := range nums {
-		if _, ok 
+	m := make(map[int]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		m[target-nums[i]] = i
 	}
 
+	result := make([]int, 2)
+
+	for index, value := range nums {
+
+		mI, ok := m[value]
+		if ok {
+
+			if mI < index {
+				result[0] = mI
+				result[1] = index
+				break
+			} else if mI > index {
+				result[0] = index
+				result[1] = mI
+				break
+			}
+
+		}
+
+	}
+
+	return result
 }
